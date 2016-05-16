1b55c9d438ced86c3571d706f14dd2e55fa435ed
Alexandre <>
2016-05-16 18:48:11 -0300

    Comentando Mensagem de Alexandre

---
+++ src/controleconfiguracao/ControleConfiguracao.java
@@ -0,0 +1,11 @@
+package controleconfiguracao;
+
+
+public class ControleConfiguracao {
+
+    
+    public static void main(String[] args) {
+        System.out.println ("Teste git: ");
+    }
+    
+}
