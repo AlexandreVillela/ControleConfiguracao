d537468c89e867494ab1804fc0d6073f5841567c
Alexandre Freitas <>
2016-05-31 20:18:24 -0300

    Merge origin/master

---
+++ src/controleconfiguracao/Projeto.java
@@ -0,0 +1,25 @@
+package controleconfiguracao;
+
+/**
+ *
+ * @author alexandre
+ */
+public class Projeto {
+    private static GrupoDeUsuarioControl controladorGrupoDeUsuario;
+        
+    public static GrupoDeUsuarioControl getInstance(){
+    if(controladorGrupoDeUsuario == null){
+    controladorGrupoDeUsuario = new GrupoDeUsuarioControl();
+    return controladorGrupoDeUsuario;
+    }
+    return controladorGrupoDeUsuario;
+    }
+    public void cadastrarGrupoDeUsuario(){
+    }
+    public void deletarGrupoDeUsuario(){
+    }
+    public void editarGrupoDeUsuario(){
+    }
+    public void listarGrupoDeUsuario(){
+}
+}
